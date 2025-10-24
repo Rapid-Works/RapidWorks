ba53b54a04913b9f2bd112bb50d9bdc54302dc30
Samuel Donkor <calvinsamueldonkor@gmail.com>
2025-10-24 16:54:36 +0000

    fix build errors

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {
-    root: '/Users/mac/Downloads/Work/Beta/landingpage/nextjs-app'
-  }
 };
 
 export default nextConfig;
